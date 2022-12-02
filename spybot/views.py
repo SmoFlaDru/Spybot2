441b352745fa60e8671fcaf4fd33693b9b011911
Benno <bensge@me.com>
2022-12-02 15:37:03 +0100

    Add home page showing list of channels

--- spybot/views.py
+++ spybot/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from spybot.models import TSChannel
 
 
 def index(request):
     return HttpResponse("Spybot is here")
+
+
+def home(request):
+    channel_list = TSChannel.objects.all()
+    template = loader.get_template('home.html')
+    context = { 'channel_list': channel_list }
+    return HttpResponse(template.render(context, request))
