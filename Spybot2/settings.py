8b1f666debfcdc89855f06f971e4c1d3ec3cac4d
Benno <bensge@me.com>
2022-12-02 14:44:59 +0100

    Make our app first in list

--- Spybot2/settings.py
+++ Spybot2/settings.py
@@ -39,13 +39,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'spybot',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'spybot',
 ]
 
 MIDDLEWARE = [
