205f67142bbe9b78159ce6b2d081bb961f9a7e81
Justus <justus.fab@gmail.com>
2022-12-03 19:00:40 +0100

    updated workflow script once more

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,10 @@ cd "$HOME/Spybot2" || exit || return 1
 echo "starting to deploy..."
 
 ls -al
+
 echo "test" > .env
 
 git pull origin master
 
 # restart spybot service here
-systemctl restart spybot2.service
+#systemctl restart spybot2.service
