d17f9f521ed3861aca4bac6807df4411aed86b6f
Benno <benno@bensge.com>
2022-12-10 17:10:43 +0100

    Automatic OS theme detection and switching

--- spybot/static/theme.js
+++ spybot/static/theme.js
@@ -5,21 +5,35 @@
  */
 
 const themeStorageKey = 'tablerTheme'
-const defaultTheme = 'light'
 let selectedTheme
 
-// https://stackoverflow.com/a/901144
-const params = new Proxy(new URLSearchParams(window.location.search), {
-	 get: (searchParams, prop) => searchParams.get(prop),
-});
+function configureTheme() {
+	console.log("configuring theme")
+	// https://stackoverflow.com/a/901144
+	const params = new Proxy(new URLSearchParams(window.location.search), {
+		get: (searchParams, prop) => searchParams.get(prop),
+	});
 
-if (!!params.theme) {
-	 localStorage.setItem(themeStorageKey, params.theme)
-	 selectedTheme = params.theme
-} else {
-	 const storedTheme = localStorage.getItem(themeStorageKey)
-	 selectedTheme = storedTheme ? storedTheme : defaultTheme
+	const isSystemDark = window.matchMedia("(prefers-color-scheme: dark)").matches
+	const defaultTheme = isSystemDark ? 'dark' : 'light'
+
+	if (!!params.theme) {
+		isParamDark = params.theme === 'dark'
+
+		if (isParamDark !== isSystemDark)
+			localStorage.setItem(themeStorageKey, params.theme)
+		else
+			localStorage.removeItem(themeStorageKey)
+		selectedTheme = params.theme
+	} else {
+		const storedTheme = localStorage.getItem(themeStorageKey)
+		selectedTheme = storedTheme ? storedTheme : defaultTheme
+	}
+
+	document.body.classList.remove('theme-dark', 'theme-light');
+	document.body.classList.add(`theme-${selectedTheme}`);
 }
 
-document.body.classList.remove('theme-dark', 'theme-light');
-document.body.classList.add(`theme-${selectedTheme}`);
+window.matchMedia("(prefers-color-scheme: dark)").onchange = configureTheme
+// configure theme now
+configureTheme()
