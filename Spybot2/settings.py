847d33a3b49f146fcfbbea050f5cb60a23fba271
Benno <benno@bensge.com>
2023-01-11 21:57:33 +0100

    Add .env option to disable the recorder

--- Spybot2/settings.py
+++ Spybot2/settings.py
@@ -27,6 +27,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+RECORDER_ENABLED = env.bool('RECORDER_ENABLED', True)
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
