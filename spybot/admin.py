b355e23edba67ac6574f8cdd56967131972d0013
Benno <benno@bensge.com>
2023-07-02 17:23:48 +0200

    Add Admin for MergedUser

--- spybot/admin.py
+++ spybot/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from spybot.models import NewsEvent
+from spybot.models import NewsEvent, TSUser, MergedUser
 
 
 # Register your models here.
 @admin.register(NewsEvent)
 class NewsEventAdmin(ModelAdmin):
     list_display = ('text', 'website_link', 'date')
+
+
+@admin.register(TSUser)
+class TSUserAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(MergedUser)
+class MergedUserAdmin(ModelAdmin):
+    def has_delete_permission(self, request, obj=None):
+        return False
