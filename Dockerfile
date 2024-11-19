8b0a6fded18584ec184aa156acc2d09ef486747e
Benno <benno@bensge.com>
2024-11-19 23:18:56 +0100

    First MVP docker-compose with Caddy serving static files, Gunicorn serving Django requests. Still missing: Extra process for running Spybot Recorder continuously.

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,26 @@
-FROM python:3.12.5-bookworm
+FROM node:23-slim AS frontend-build
+
+COPY frontend frontend
+WORKDIR frontend
+RUN npm install
+RUN npm run package
+
+FROM python:3.12-slim-bookworm
 
 WORKDIR /app
 
-ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
 
 EXPOSE 8000
 
-RUN pip install --progress-bar off uv
+# Install mysqlclient debian package dependencies
+RUN apt-get update && apt-get install -y --no-install-recommends python3-dev default-libmysqlclient-dev build-essential pkg-config && rm -rf /var/lib/apt/lists/*
+RUN pip install uv
 COPY pyproject.toml pyproject.toml
 RUN uv sync
 COPY . .
 
-CMD uv run manage.py runserver 127.0.0.1:8000
+COPY --from=frontend-build frontend/output frontend/output
+
+CMD ["sh", "run.sh"]
--- run.sh
+++ run.sh
@@ -1,33 +1,14 @@
 #!/bin/bash
 
-# install frontend dependencies
-cd frontend
-npm install
-npm run package
-cd ..
-
+ls -a
 # activate venv
-source venv/bin/activate
-
-# install project python deps
-pip3.11 install uv
-uv sync
+#.venv/bin/activate
 
 # copy static files to directory for http server
-uv run manage.py collectstatic --noinput
-
-# create django superuser if necessary.
-# Set the env vars DJANGO_SUPERUSER_{PASSWORD,USERNAME,EMAIL} when running this script
-# shellcheck disable=SC2046
-env $(grep -E '^DJANGO_SUPERUSER_(PASSWORD|USERNAME|EMAIL)' .env | xargs) python manage.py createsuperuser --noinput
+.venv/bin/python manage.py collectstatic --noinput
 
 # run DB migrations if necessary
-uv run manage.py migrate
-
-# start cronjobs with crontab using django-crontab
-uv run manage.py crontab add
-# run twice in case jobs have been removed. See: https://github.com/kraiz/django-crontab/blob/master/django_crontab/crontab.py#L209
-uv run manage.py crontab add
+.venv/bin/python manage.py migrate
 
 # run django app
-uv run manage.py runserver 127.0.0.1:8000
+.venv/bin/gunicorn -w 2 --bind 0.0.0.0:8000 Spybot2.wsgi
