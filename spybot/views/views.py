d753f272b787b468c96d3edb134e48dd9ff94ec9
Justus <justus.fab@gmail.com>
2025-06-21 17:17:13 +0200

    fix function name

--- spybot/views/views.py
+++ spybot/views/views.py
@@ -221,7 +221,7 @@ def user(request, user_id: int):
     game_id = 0
 
     if u.get("online") == 1:
-        game_id, game_name = get_steam_game(MergedUser.objects.get(id=user_id))
+        game_id, game_name = get_steam_games(MergedUser.objects.get(id=user_id))
 
     months = visualization.user_month_activity(user_id)
 
